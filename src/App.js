f14305c1becb16d37018c26d087ff87a79c11d49
TheNootLinja <thenootlinja@gmail.com>
2020-11-19 21:21:20 -0500

    Add Card and CardList components to the code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component'
+
 import './App.css';
+
+// https://jsonplaceholder.typicode.com/users
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: Math.random(),
-        },
-        {
-          name: 'Dracula',
-          id: Math.random(),
-        },
-        {
-          name: 'Zombie',
-          id: Math.random(),
-        },
-      ]
+      monsters: [],
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ monsters: users }))
   }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{ monster.name }</h1>
-          ))}
+        <CardList monsters = {this.state.monsters}/>
       </div>
     )
   }
