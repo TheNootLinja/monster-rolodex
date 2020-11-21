a074d448945fed743770fd307f015c612bff32b7
TheNootLinja <thenootlinja@gmail.com>
2020-11-20 22:16:43 -0500

    Add SearchBox component, the styling for SearchBox, and implemented the search functionality using the component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/SearchBox.component'
 
 import './App.css';
 
@@ -12,7 +13,8 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-    }
+      searchField: '',
+    };
   }
 
   componentDidMount() {
@@ -20,10 +22,26 @@ class App extends Component {
     .then(res => res.json())
     .then(users => this.setState({ monsters: users }))
   }
+
+  // It is important to remember that when we use an arrow function
+  // as we are below, we are automatically binding "this" to the
+  // correct contect automatically.
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
-      <div className="App">
-        <CardList monsters = {this.state.monsters}/>
+      <div className='App'>
+        <SearchBox 
+        placeholder = 'Search Monsters'
+        handleChange = {this.handleChange}
+        />
+        <CardList monsters = {filteredMonsters} />
       </div>
     )
   }
--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -3,7 +3,7 @@ import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => (
-    <div class="card-list">
+    <div className="card-list">
         {props.monsters.map(monster => (
             <Card key={monster.id} monster={monster} />
         ))}
